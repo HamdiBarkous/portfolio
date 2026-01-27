0c08230cfa928302976dc707352413c77c58d2a6
Hamdi Barkous <hamdibarkous10@gmail.com>
2026-01-27 01:46:34 +0100

    add: analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { ThemeProvider } from "@/components/ThemeProvider"; // Import ThemeProvider
@@ -38,6 +39,7 @@ export default function RootLayout({
           <main className="flex-grow">{children}</main>
           <Footer />
         </ThemeProvider>
+        <Analytics />
       </body>{/* Ensure no whitespace/newline after body */}
     </html>
   );
